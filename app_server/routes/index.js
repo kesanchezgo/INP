43e1e0868ddcbcb304fca9e2840f64278e607c49
kesanchezgo <kevin.08.24.11@gmail.com>
2016-09-25 22:21:26 -0500

    Se agrego seccion perfil

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,7 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 var ctrlRegister =	require('../controllers/register');
 var ctrlTeam =	require('../controllers/team');
+var ctrlUser = require('../controllers/user');
 
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
@@ -11,6 +12,7 @@ router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 router.get('/register',	ctrlRegister.register);
 router.get('/team',	ctrlTeam.teamInfo);
+router.get('/profile',	ctrlUser.profile);
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 
